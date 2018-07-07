78a419f3f874192dcdc667fa2a252170d59c4e37
badlogic <contact@badlogicgames.com>
2018-07-07 13:56:23 +0200

    New site changes (post, project, music).

--- reload.sh
+++ reload.sh
@@ -2,4 +2,4 @@
 git commit -am "New site changes (post, project, music)."
 set -e
 git push
-curl -X POST -F "password=$MARIOSLAB_PWD" http://localhost:8000/api/reload
+curl -X POST -F "password=$MARIOSLAB_PWD" https://marioslab.io/api/reload
