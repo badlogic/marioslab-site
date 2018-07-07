8a5c876ce33869fe8a0596b73cb39f5b86918fe2
badlogic <contact@badlogicgames.com>
2018-07-07 13:55:36 +0200

    New site changes (post, project, music).

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 git commit -am "New site changes (post, project, music)."
 set -e
 git push
-curl -X POST -F "password=$MARIOSLAB_PWD" https://localhost:8000/api/reloadstatic
+curl -X POST -F "password=$MARIOSLAB_PWD" http://localhost:8000/api/reloadstatic
