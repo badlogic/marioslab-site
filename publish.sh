a940dfdbd4fcb26b1ed95813325107c47e75b8f2
badlogic <badlogicgames@gmail.com>
2021-02-21 17:15:50 +0100

    New site changes (post, project, music).

--- publish.sh
+++ publish.sh
@@ -2,5 +2,6 @@
 git commit -am "New site changes (post, project, music)."
 set -e
 git push
-mvn clean package -U
+mvn package -U
 java -jar target/marioslab.jar -i site -o output -w -d -go -p "foobar"
+rsync -avz output/ marioslab.io:/home/badlogic/marioslab.io/data/web/
