c8e4441bc9d443e8f5a73f11451acddfb9c5ab56
badlogic <badlogicgames@gmail.com>
2021-02-21 17:12:01 +0100

    New site changes (post, project, music).

--- src/main/java/io/marioslab/MariosLab.java
+++ src/main/java/io/marioslab/MariosLab.java
@@ -22,94 +22,95 @@ import io.marioslab.basis.site.processors.TemplateFileProcessor.BuiltinFunctionP
 import io.marioslab.processors.ImageCropProcessor;
 
 public class MariosLab {
-	private static Map<WebSocketHandler, WsSession> wsClients = new ConcurrentHashMap<>();
+    private static Map<WebSocketHandler, WsSession> wsClients = new ConcurrentHashMap<>();
 
-	public static void main (String[] cliArgs) {
-		Arguments args = BasisSite.createDefaultArguments();
-		StringArgument passwordArg = args.addArgument(new StringArgument("-p", "Password that must be provided for reload endpoints.", "<password>", false));
-		Argument generateOnlyArg = args.addArgument(new Argument("-go", "Generate static assets, then terminate.", true));
-		Argument reloadArg = args
-			.addArgument(new Argument("-r", "Whether to tell any browser websocket clients to\nreload the site when the output was\nre-generated", true));
+    public static void main(String[] cliArgs) {
+        Arguments args = BasisSite.createDefaultArguments();
+        StringArgument passwordArg = args.addArgument(new StringArgument("-p", "Password that must be provided for reload endpoints.", "<password>", false));
+        Argument generateOnlyArg = args.addArgument(new Argument("-go", "Generate static assets, then terminate.", true));
+        Argument reloadArg = args
+                .addArgument(new Argument("-r", "Whether to tell any browser websocket clients to\nreload the site when the output was\nre-generated", true));
 
-		ParsedArguments parsed;
-		byte[] password;
-		BasisSite site;
-		try {
-			parsed = args.parse(cliArgs);
-			password = parsed.getValue(passwordArg).getBytes("UTF-8");
-			site = new BasisSite(parsed);
-			site.replaceProcessor(new TemplateFileProcessor(Arrays.asList( (file, context) -> {
-				context.set("reloadWS", parsed.has(reloadArg));
-			}, new BuiltinFunctionProvider(site.getGenerator()))));
-			site.addProcessor(new ImageCropProcessor());
-		} catch (Throwable e) {
-			Log.error(e.getMessage());
-			Log.debug("Exception", e);
-			args.printHelp(System.out);
-			System.exit(-1);
-			return; // never reached
-		}
+        ParsedArguments parsed;
+        byte[] password;
+        BasisSite site;
+        try {
+            parsed = args.parse(cliArgs);
+            password = parsed.getValue(passwordArg).getBytes("UTF-8");
+            site = new BasisSite(parsed);
+            site.replaceProcessor(new TemplateFileProcessor(Arrays.asList((file, context) -> {
+                context.set("reloadWS", parsed.has(reloadArg));
+            }, new BuiltinFunctionProvider(site.getGenerator()))));
+            site.addProcessor(new ImageCropProcessor());
+        } catch (Throwable e) {
+            Log.error(e.getMessage());
+            Log.debug("Exception", e);
+            args.printHelp(System.out);
+            System.exit(-1);
+            return; // never reached
+        }
 
-		if (parsed.has(generateOnlyArg)) {
-			site.generate(() -> {});
-			System.exit(0);
-		}
+        if (parsed.has(generateOnlyArg)) {
+            site.generate(() -> {
+                System.exit(0);
+            });
+        }
 
-		Thread generatorThread = new Thread((Runnable) () -> {
-			try {
-				site.generate( () -> {
-					for (WsSession session : wsClients.values()) {
-						session.send("Reload");
-					}
-				});
-			} catch (Throwable t) {
-				Log.error(t.getMessage());
-				Log.debug("Exception", t);
-			}
-		});
-		generatorThread.setDaemon(true);
-		generatorThread.start();
+        Thread generatorThread = new Thread((Runnable) () -> {
+            try {
+                site.generate(() -> {
+                    for (WsSession session : wsClients.values()) {
+                        session.send("Reload");
+                    }
+                });
+            } catch (Throwable t) {
+                Log.error(t.getMessage());
+                Log.debug("Exception", t);
+            }
+        });
+        generatorThread.setDaemon(true);
+        generatorThread.start();
 
-		Javalin app = Javalin.create().enableDynamicGzip().enableStaticFiles("output", Location.EXTERNAL).port(8000).start();
+        Javalin app = Javalin.create().enableDynamicGzip().enableStaticFiles("output", Location.EXTERNAL).port(8000).start();
 
-		if (parsed.has(reloadArg)) {
-			app.ws("/api/reloadws", ws -> {
-				Log.info("Setting up WebSocket reloading");
+        if (parsed.has(reloadArg)) {
+            app.ws("/api/reloadws", ws -> {
+                Log.info("Setting up WebSocket reloading");
 
-				ws.onConnect(session -> {
-					Log.info("WebSocket client connected");
-					wsClients.put(ws, session);
-				});
+                ws.onConnect(session -> {
+                    Log.info("WebSocket client connected");
+                    wsClients.put(ws, session);
+                });
 
-				ws.onClose( (session, statusCode, reason) -> {
-					Log.info("WebSocket client disconnected");
-					wsClients.remove(ws);
-				});
+                ws.onClose((session, statusCode, reason) -> {
+                    Log.info("WebSocket client disconnected");
+                    wsClients.remove(ws);
+                });
 
-				ws.onError( (session, throwable) -> {
-					Log.info("WebSocket client disconnected");
-					wsClients.remove(ws);
-				});
-			});
-		}
+                ws.onError((session, throwable) -> {
+                    Log.info("WebSocket client disconnected");
+                    wsClients.remove(ws);
+                });
+            });
+        }
 
-		app.post("/api/reloadstatic", ctx -> {
-			String pwd = ctx.formParam("password");
-			if (MessageDigest.isEqual(pwd.getBytes(), password)) {
-				new ProcessBuilder().command("git", "pull").start();
-				Log.info("Got new static content.");
-				ctx.response().getWriter().println("OK.");
-			}
-		});
+        app.post("/api/reloadstatic", ctx -> {
+            String pwd = ctx.formParam("password");
+            if (MessageDigest.isEqual(pwd.getBytes(), password)) {
+                new ProcessBuilder().command("git", "pull").start();
+                Log.info("Got new static content.");
+                ctx.response().getWriter().println("OK.");
+            }
+        });
 
-		app.post("/api/reload", ctx -> {
-			String pwd = ctx.formParam("password");
-			if (MessageDigest.isEqual(pwd.getBytes(), password)) {
-				ctx.response().getWriter().println("OK.");
-				ctx.response().getWriter().flush();
-				Log.info("Got an update. Shutting down.");
-				System.exit(-1);
-			}
-		});
-	}
+        app.post("/api/reload", ctx -> {
+            String pwd = ctx.formParam("password");
+            if (MessageDigest.isEqual(pwd.getBytes(), password)) {
+                ctx.response().getWriter().println("OK.");
+                ctx.response().getWriter().flush();
+                Log.info("Got an update. Shutting down.");
+                System.exit(-1);
+            }
+        });
+    }
 }
