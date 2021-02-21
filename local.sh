3fa9cc05e75c762bdabbc1dc7f0d9675ac8995c7
Mario Zechner <badlogicgames@gmail.com>
2021-02-21 20:56:33 +0100

    Fix up local.sh.

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git pull
 mvn clean package -U
-java -jar target/marioslab.jar -i site -o output -w -d -r -p $MARIOSLAB_PWD
+java -jar target/marioslab.jar -i site -o output -w -d -r -p ""
