3456f9e0f1de3117e0ac4421e3d29af0db20e296
Mario Zechner <badlogicgames@gmail.com>
2021-03-02 21:37:57 +0100

    Fixced up links got GitHub repo, direclty linking to blobs of a specific commit.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-git commit -am "New site changes (post, project, music)."
+if [ $# -lt 1 ]; then
+	echo "Usage: publish.sh <commit-message>"
+    exit 1;
+fi
+
 set -e
+git commit -am "$1"
 git push
 mvn package
 java -jar target/marioslab.jar -i site -o output -w -d -go -p "foobar"
