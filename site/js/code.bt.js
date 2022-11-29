83d8be94438c74acc77f91f44aec07fdd26a5c49
Mario Zechner <badlogicgames@gmail.com>
2022-11-30 00:56:40 +0100

    New blog posts.

--- site/js/code.bt.js
+++ site/js/code.bt.js
@@ -1,6 +1,8 @@
 {{include raw "_highlight.pack.js"}}
+{{include raw "_highlightjs-line-numbers.min.js"}}
 
-hljs.initHighlightingOnLoad();
+hljs.highlightAll();
+hljs.initLineNumbersOnLoad();
 
 {{if reloadWS}}
 var url = window.location;
