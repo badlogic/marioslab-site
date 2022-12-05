6c04be75f26df5d565224de4d7a78ebccc5a8233
Mario Zechner <badlogicgames@gmail.com>
2022-12-06 00:51:51 +0100

    more words.

--- site/js/code.bt.js
+++ site/js/code.bt.js
@@ -35,3 +35,48 @@ function tableOfContents() {
 		toc.appendChild(tocEntry);
 	}
 }
+
+function q5Diagram(width, height, divId) {
+	let q5 = new Q5("offscreen");
+	let canvas = q5.createCanvas(width, height);
+	q5.pixelDensity(2);
+	canvas.style["max-width"] = "100%";
+	canvas.style.width = "";
+	canvas.style.height = "";
+	document.getElementById(divId).appendChild(canvas);
+
+	q5.textSize(16);
+	q5.textAlign(q5.CENTER, q5.CENTER)
+	q5.noLoop();
+
+	let blockSize = 40;
+	q5.blockSize = (bs) => {
+		if (bs !== undefined) blockSize = bs;
+		return bs;
+	}
+
+	q5.grid = (x, y, w, h, stroke) => {
+		stroke = stroke !== undefined ? stroke : "black";
+		q5.noFill()
+		q5.stroke(stroke)
+		for (let yy = 0; yy < h; yy++)
+			for(let xx = 0; xx < w; xx++)
+				q5.rect((xx + x) * blockSize, (yy + y) * blockSize, blockSize, blockSize);
+	}
+
+	q5.block = (x, y, fill, stroke) => {
+		stroke = stroke !== undefined ? stroke : "black";
+		q5.fill(fill)
+		q5.stroke(stroke);
+		q5.rect(x * blockSize, y * blockSize, blockSize, blockSize);
+	}
+
+	q5.blockText = (text, x, y, fill) => {
+		x += 0.5;
+		y += 0.5;
+		q5.fill(fill)
+		q5.noStroke();
+		q5.text(text, x * blockSize, y * blockSize);
+	}
+	return q5;
+}
